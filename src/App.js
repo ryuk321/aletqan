102a5ed713d63ea19b1ac5f55b6b7fa29e0b1a40
ryuk321 <princeshrestha596@gmail.com>
2023-03-09 11:12:20 +0545

    Neavigation bar update

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
                     element={<Contacts />}
                 />
             </Routes>
-            {/* <Footer /> */}
+            <Footer />
         </>
     );
 }
